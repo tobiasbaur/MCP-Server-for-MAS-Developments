bd527012eb03eefcbb0599c2e6ab8032c0f1002a
Dr. Tobias Baur <tobiasbaur@users.noreply.github.com>
2025-02-09 12:15:54 +0100

    improve completions api works with continue, update readme

--- agents/OpenAI_Compatible_API_Agent/Python/open_ai_helper.py
+++ agents/OpenAI_Compatible_API_Agent/Python/open_ai_helper.py
@@ -34,6 +34,19 @@ class ChatCompletionRequest(BaseModel):
     groups: Optional[object] = None
 
 
+
+class CompletionRequest(BaseModel):
+    model: Optional[str] = "PGPT - Mistral NeMo 12B"
+    max_tokens: Optional[int] = 64000
+    temperature: Optional[float] = 0 #Not used atm
+    stream: Optional[bool] = False
+    response_format: Optional[object] = None
+    tools: Optional[object] = None
+    groups: Optional[object] = None
+    prompt: str = ""
+
+
+
 def num_tokens(user_input, answer):
     num_tokens_request = num_tokens_from_string(user_input, "o200k_base")
     num_tokens_reply = num_tokens_from_string(answer, "o200k_base")
@@ -49,8 +62,10 @@ def num_tokens_from_string(string: str, encoding_name: str) -> int:
 
 def _resp_sync(response: json, request):
     user_input = ""
+    reply = {}
     for message in request.messages:
         user_input += json.dumps({'role': message.role, 'content': message.content})
+        reply = [{"message": Message(role="assistant", content=response["answer"])}]
 
     num_tokens_request, num_tokens_reply, num_tokens_overall = num_tokens(user_input, response["answer"])
 
@@ -58,12 +73,15 @@ def _resp_sync(response: json, request):
     if "sources" in response:
         citations = response["sources"]
 
+
+
     return {
         "id": response["chatId"],
         "object": "chat.completion",
         "created": time.time(),
         "model": request.model,
-        "choices": [{"message": Message(role="assistant", content=response["answer"])}],
+        "choices": reply,
+
         "citations": citations,
         "usage": {
             "prompt_tokens": num_tokens_request,
@@ -73,12 +91,11 @@ def _resp_sync(response: json, request):
     }
 
 
-
-
 async def _resp_async_generator(response: json, request):
     user_input = ""
     for message in request.messages:
-        user_input += json.dumps({'role': message.role, 'content': message.content})
+            user_input += json.dumps({'role': message.role, 'content': message.content})
+
 
     num_tokens_request, num_tokens_reply, num_tokens_overall = num_tokens(user_input, response["answer"])
 
@@ -104,6 +121,77 @@ async def _resp_async_generator(response: json, request):
         yield f"data: {json.dumps(chunk)}\n\n"
         await asyncio.sleep(0.05)
     yield "data: [DONE]\n\n"
+
+
+
+
+# Legacy Completions API
+def _resp_sync_completions(response: json, request):
+    user_input = request.prompt
+    reply = [{"text": response["answer"],
+            "index": 0,
+            "logprobs": None,
+            "finish_reason": "length"}]
+
+    num_tokens_request, num_tokens_reply, num_tokens_overall = num_tokens(user_input, response["answer"])
+
+    citations = []
+    if "sources" in response:
+        citations = response["sources"]
+
+
+
+    return {
+        "id": response["chatId"],
+        "object": "text_completion",
+        "created": time.time(),
+        "model": request.model,
+        "choices": reply,
+        "citations": citations,
+        "usage": {
+            "prompt_tokens": num_tokens_request,
+            "completion_tokens": num_tokens_reply,
+            "total_tokens": num_tokens_overall
+        }
+    }
+
+async def _resp_async_generator_completions(response: json, request):
+
+    user_input = request.prompt
+    num_tokens_request, num_tokens_reply, num_tokens_overall = num_tokens(user_input, response["answer"])
+
+    tokens = response["answer"].split(" ")
+    citations = []
+    if "sources" in response:
+        citations = response["sources"]
+
+
+    for i, token in enumerate(tokens):
+        chunk = {
+            "id": i,
+            "object": "text_completion",
+            "created": time.time(),
+            "model": request.model,
+            "choices": [
+                {
+                  "text": token + " ",
+                  "index": 0,
+                  "logprobs": None,
+                  "finish_reason": None
+                }
+            ],
+
+            "citations": citations,
+            "usage": {
+                "prompt_tokens": num_tokens_request,
+                "completion_tokens": num_tokens_reply,
+                "total_tokens": num_tokens_overall
+            }
+        }
+        yield f"data: {json.dumps(chunk)}\n\n"
+        await asyncio.sleep(0.05)
+    yield "data: [DONE]\n\n"
+
 
 
 
